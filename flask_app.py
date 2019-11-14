8498b50793a2a18c3ae7aecd4c61fd2077e6b141
thecosmos <deepakrajko14@gmail.com>
2019-11-14 14:21:03 +0530

    Use MongoDB URI from environment

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import os
 import random
 import string
 
@@ -11,10 +12,7 @@ app = Flask(__name__)
 cors = CORS(app)
 api = Api(app)
 app.secret_key = b"'\x14Q\x17\xda\xc6\x8f\x01V\x9et\xdf\x81\xa0$m"
-db_client = MongoClient("mongodb://thecosmos:I2vhyffcplsVoAbr@tc-discord-bot-shard-00-00-i4l5o.mongodb.net:27017,"
-                        "tc-discord-bot-shard-00-01-i4l5o.mongodb.net:27017,"
-                        "tc-discord-bot-shard-00-02-i4l5o.mongodb.net:27017/test?ssl=true&replicaSet=tc-discord-bot"
-                        "-shard-0&authSource=admin")
+db_client = MongoClient(os.environ.get("MONGODB_URI"))
 db = db_client["instant-notes"]
 
 
