b536fdd6d9fca0012f0f8e7cd89f99f955d30e58
thec0sm0s <deepakrajko14@gmail.com>
2020-10-10 02:04:52 +0530

    Use serializer for preferences

--- app/api/views/user/serializer.py
+++ app/api/views/user/serializer.py
@@ -1,9 +1,17 @@
 from .. import SerializerBaseSchema
 
+import json
+
 from marshmallow import fields
+
+
+class UserPreferences(fields.Dict):
+
+    def _serialize(self, value, *args, **kwargs):
+        return super()._serialize(json.loads(value.to_json()), *args, **kwargs)
 
 
 class UserSchema(SerializerBaseSchema):
 
     username = fields.String(required=True)
-    preferences = fields.Dict(required=True)
+    preferences = UserPreferences(required=True)
