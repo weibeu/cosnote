362f4f43677e900cb1ed50c790522e71cf92b95e
thec0sm0s <deepakrajko14@gmail.com>
2020-10-12 15:21:17 +0530

    Update configs.py

--- application/configs.py
+++ application/configs.py
@@ -5,7 +5,7 @@ import dotenv
 dotenv.load_dotenv(".env")
 
 
-MONGODB_DATABASE = "instant_notes"
+MONGODB_DATABASE = "cosnote"
 MONGODB_URI = os.getenv("MONGODB_URI")
 
 SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
